f8ed3b01134362fc27e0af58f19ad0f399868674
Faiza Siddiqui <faizasiddiqui@uregina.ca>
2021-10-18 08:23:24 +0300

    adding sin function

--- Section_2_Unit_Test_sin_cos.cpp
+++ Section_2_Unit_Test_sin_cos.cpp
@@ -5,6 +5,52 @@
 
 using namespace std;
 
+/** Compute the sine of an angle in degrees.
+
+    @param x The angle in degrees
+
+    @return The sine of x
+
+*/
+int sin0to45(int);
+
+int sin45to90(int);
+
+int sin(int x) {
+
+  if (x < 0) {
+
+    x = -x;
+
+  }
+
+  x = x % 360;
+
+  if (0 <= x && x <= 45) {
+
+    return sin0to45(x);
+
+  }
+
+  else if (45 <= x && x <= 90) {
+
+    return sin45to90(x);
+
+  }
+
+  else if (90 <= x && x <= 180) {
+
+    return sin(180 - x);
+
+  }
+
+  else {
+
+    return -sin(x - 180);
+
+  }
+
+}
 
 int main()
 
